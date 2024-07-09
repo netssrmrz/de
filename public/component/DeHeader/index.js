51dd4c78b4a35023be2938838e5f05ddd47fe85c
Steven Ramirez <netssrmrz@yahoo.com.au>
2024-07-09 22:09:41 +1000

    updates

--- public/component/DeHeader/index.js
+++ public/component/DeHeader/index.js
@@ -136,7 +136,7 @@ class DeHeader extends HTMLElement
         <li><a href="index.html">Home</a></li>
         <li><a href="index.html#clients">Clients</a></li>
         <li><a href="index.html#services">Services</a></li>
-        <li><a href="dedial-video.html">DeDial Component</a></li>
+        <li><a href="dedial-index.html">DeDial Component</a></li>
         <li><a href="dedial-docs.html">DeDial Reference</a></li>
       </ul>
 
